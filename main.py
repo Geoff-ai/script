c7280d2d32a22addde98df6ed2d5829861830f96
Geoff-ai <geoffroymarol@gmail.com>
2024-10-08 16:35:43 +0200

    Update main.py

--- main.py
+++ main.py
@@ -6,18 +6,10 @@ PAGES = {
     "Analyse description des produits": description_produits.traiter_fichier_excel
 }
 
-# Titre principal
-st.sidebar.title("Interface de description de produits")
+# Menu latéral
+st.sidebar.title("Menu")
+selection = st.sidebar.radio("Choisissez une option", list(PAGES.keys()))
 
-# Sous-titre et choix des scripts
-st.sidebar.subheader("Les scripts disponibles")
-selection = st.sidebar.radio('', list(PAGES.keys()), index=0)
-
-# Affichage du script sélectionné
+# Appelle la fonction de la page sélectionnée
 page = PAGES[selection]
-
-# Appel de la fonction app() du module sélectionné
-fichier = st.file_uploader("Choisissez un fichier Excel", type="xlsx")
-
-if fichier:
-    page(fichier)
+page()
