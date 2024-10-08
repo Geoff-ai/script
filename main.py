06b749c935a7a5775d7c58838337c8fae33a408d
Geoff-ai <geoffroymarol@gmail.com>
2024-10-08 16:24:20 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,18 +3,21 @@ from scripts import description_produits
 
 # Configuration des pages
 PAGES = {
-    "Analyse description des produits": description_produits.app
+    "Analyse description des produits": description_produits.traiter_fichier_excel
 }
 
 # Titre principal
-st.sidebar.title("roykyserpking")
+st.sidebar.title("Interface de description de produits")
 
 # Sous-titre et choix des scripts
-st.sidebar.subheader("Les scripts")
-selection = st.sidebar.radio("", list(PAGES.keys()), index=0)
+st.sidebar.subheader("Les scripts disponibles")
+selection = st.sidebar.radio('', list(PAGES.keys()), index=0)
 
 # Affichage du script sélectionné
 page = PAGES[selection]
 
 # Appel de la fonction app() du module sélectionné
-page()  # Exécute la fonction app() de description_produits
+fichier = st.file_uploader("Choisissez un fichier Excel", type="xlsx")
+
+if fichier:
+    page(fichier)
